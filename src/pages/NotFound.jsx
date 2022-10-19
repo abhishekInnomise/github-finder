a462a09224ddcdb974a0a863893c667e7462f5ba
Abhishek Dhapare <abhishek.d@innomise.com>
2022-10-19 11:34:12 +0530

    Not Found Page message changed

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -6,7 +6,7 @@ function NotFound() {
     <div className='hero'>
       <div className='text-center hero-content'>
         <div className='max-w-lg'>
-          <h1 className='text-8xl font-bold mb-8'>Oops!</h1>
+          <h1 className='text-8xl font-bold mb-8'>Oops! Something went wrong!</h1>
           <p className='text-5xl mb-8'>404 - Page Not Found!</p>
           <Link className='btn btn-primary btn-lg' to='/'>
             <FaHome className='mr-2' />
